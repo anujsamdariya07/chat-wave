1fb72076debeda7d83f91cac94bf1eafca463684
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-12-04 20:05:01 +0530

    setting up message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -13,6 +14,7 @@ const PORT = process.env.PORT;
 app.use(cookieParser())
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running at PORT:,', PORT);
